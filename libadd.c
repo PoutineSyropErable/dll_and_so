e759fb4b240b9f89c4a9218b2cf7de300ef2aae9
François Pagé <francois.page@mail.mcgill.ca>
2024-08-12 17:46:15 -0400

    Created the code from the tutorial with my add function added

---
+++ libadd.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <math.h>
+
+int add( int a, int b) {
+
+
+	return a + b ;
+}
+
+int main() {
+
+	
+	printf("Hello world\n");
+
+	printf("5 + 3 = %d\n", add(5,3));
+
+	//Let's create a random number:
+	time_t current_time;
+	srand(unsigned) time(&currentTime));
+	int randomNumber = rand();
+
+	double squareRoot = sqrt(randomNumber);
+
+	printf("Square root of %d is %f\n",randomNumber, squareRoot);
+
+
+	return 0;
+}
