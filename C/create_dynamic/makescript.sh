113e3830983e03353965210cbb70d741bb0f1c9a
François Pagé <francois.page@mail.mcgill.ca>
2024-08-13 00:34:46 -0400

    Got a general and safe working C with dynamic linker

--- C/create_dynamic/makescript.sh
+++ C/create_dynamic/makescript.sh
@@ -2,11 +2,22 @@
 
 
 # -O0 is for no optimisation ,it's so reading the object dump and learning x86 is easier
-gcc -fpic -O0 -c src/math/addNumbers.c -o obj/math/addNumbers.o
-gcc -fpic -O0 -c src/math/subNumbers.c -o obj/math/subNumbers.o
+gcc -fpic -O0 -c src/math/addNumbers.c -o obj/math/addNumbers.o -I include
+gcc -fpic -O0 -c src/math/subNumbers.c -o obj/math/subNumbers.o -I include
 #use -fPICif the compiler complains if the address shift is too big for 32 bit, slower code though
 
-gcc -fpic -shared -o lib/libmath.so obj/math/addNumbers.o obj/math/subNumbers.o  
+# Gepeto said adding -I include might help, so I'm doing it
+
+#Create the shared library
+gcc -fpic -shared -o lib/libmath.so.1.0.0 obj/math/addNumbers.o obj/math/subNumbers.o  
+
+
+ln -sf "$PWD/lib/libmath.so.1.0.0" "$PWD/lib/libmath.so.1"
+ln -sf "$PWD/lib/libmath.so.1" "$PWD/lib/libmath.so"
+
+# Real name: libmath.so.1.0.0
+# SO name: libmath.so.1
+# Linker name: libmath.so
 
 
 gcc doMath.c -o doMath -O0 -I include -L lib -lmath -Wl,-rpath,"$PWD/lib"
@@ -16,4 +27,6 @@ gcc doMath.c -o doMath -O0 -I include -L lib -lmath -Wl,-rpath,"$PWD/lib"
 
 objdump -D doMath > doMath.dump
 cp doMath.dump doMath.asm
+
+./doMath
 
--- create_static/makescript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-gcc -c src/math/addNumbers.c -o obj/math/addNumbers.o
-gcc -c src/math/subNumbers.c -o obj/math/subNumbers.o
-
-
-ar rcs lib/libmath.a obj/math/addNumbers.o obj/math/subNumbers.o
-
-gcc -Iinclude -Llib -o doMath doMath.c -lmath
-
