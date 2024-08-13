79dde8dad4fc4ab7564d52afd3d50abbe6d23180
François Pagé <francois.page@mail.mcgill.ca>
2024-08-12 22:34:30 -0400

    gcc -c is to create object files, don't be dumb

---
+++ create_static/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "mathtest.h"
+
+int main() {
+    int a = 3;
+    int b = 5;
+    printf("Sum: %d\n", addNumbers(a, b));
+    printf("Difference: %d\n", subNumbers(a, b));
+    return 0;
+}
